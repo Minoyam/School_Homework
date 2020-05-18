a05c08498cdba3b4ba6437670fe75b16c80a0e2a
mino <k3000js@naver.com>
2020-05-18 14:55:49 +0900

    23강 메뉴로 색상/도형 바꾸기

--- app/src/main/java/com/cnm/shw/draw/DrawActivity.kt
+++ app/src/main/java/com/cnm/shw/draw/DrawActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +20,8 @@ class DrawActivity : AppCompatActivity() {
     var endX = 0.0f
     var endY = 0.0f
     lateinit var myView: MyView
+    private val p = Paint()
+    private var selectFigure: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +36,7 @@ class DrawActivity : AppCompatActivity() {
                 startX = event.x
                 startY = event.y
             }
-            MotionEvent.ACTION_UP,MotionEvent.ACTION_MOVE -> {
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE -> {
                 endX = event.x
                 endY = event.y
                 myView.invalidate()
@@ -42,18 +46,41 @@ class DrawActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.im_draw_blue -> p.color = Color.BLUE
+            R.id.im_draw_green -> p.color = Color.GREEN
+            R.id.im_draw_red -> p.color = Color.RED
+            R.id.im_draw_gray -> p.color = Color.GRAY
+            R.id.im_draw_black -> p.color = Color.BLACK
+            R.id.im_draw_yellow -> p.color = Color.YELLOW
+            R.id.im_rectangle -> selectFigure = 0
+            R.id.im_circle -> selectFigure = 2
+            R.id.im_line -> selectFigure = 1
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     inner class MyView(context: Context) : View(context) {
-        private val p = Paint()
         private val s = Paint()
         override fun onDraw(canvas: Canvas?) {
             super.onDraw(canvas)
-            p.color = Color.YELLOW
             s.color = Color.BLUE
             s.textSize = 30f
-            canvas?.drawRect(startX, startY, endX, endY, p)
+            when (selectFigure) {
+                0 -> canvas?.drawRect(startX, startY, endX, endY, p)
+                1 -> canvas?.drawLine(startX, startY, endX, endY, p)
+                2 -> canvas?.drawCircle(startX, startY,100F,p)
+
+            }
             var string: String = ""
             string = "startX = $startX  startY = $startY   endX = $endX   endY = $endY"
-                canvas?.drawText(string, 10F, 1500F, s)
+            canvas?.drawText(string, 10F, 1500F, s)
 
 
         }
--- app/src/main/java/com/cnm/shw/menubar/MenuActivity.kt
+++ app/src/main/java/com/cnm/shw/menubar/MenuActivity.kt
@@ -22,33 +22,15 @@ class MenuActivity :AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         with(et_name) {
             when (item.itemId) {
-                R.id.im_blue -> {
-                    this.setBackgroundColor(android.graphics.Color.BLUE)
-                }
-                R.id.im_green -> {
-                    this.setBackgroundColor(android.graphics.Color.GREEN)
-                }
-                R.id.im_red -> {
-                    this.setBackgroundColor(android.graphics.Color.RED)
-                }
-                R.id.im_dark_gray -> {
-                    this.setBackgroundColor(android.graphics.Color.DKGRAY)
-                }
-                R.id.im_normal_gray -> {
-                    this.setBackgroundColor(android.graphics.Color.GRAY)
-                }
-                R.id.im_light_gray -> {
-                    this.setBackgroundColor(android.graphics.Color.LTGRAY)
-                }
-                R.id.im_white -> {
-                    this.setBackgroundColor(android.graphics.Color.rgb(255, 148, 0))
-                }
-                R.id.im_black -> {
-                    this.setBackgroundColor(android.graphics.Color.rgb(0, 225, 255))
-                }
-                R.id.im_yellow -> {
-                    this.setBackgroundColor(android.graphics.Color.rgb(118, 0, 145))
-                }
+                R.id.im_draw_blue ->this.setBackgroundColor(android.graphics.Color.BLUE)
+                R.id.im_draw_green -> this.setBackgroundColor(android.graphics.Color.GREEN)
+                R.id.im_draw_red -> this.setBackgroundColor(android.graphics.Color.RED)
+                R.id.im_draw_gray -> this.setBackgroundColor(android.graphics.Color.DKGRAY)
+                R.id.im_draw_black -> this.setBackgroundColor(android.graphics.Color.GRAY)
+                R.id.im_draw_yellow -> this.setBackgroundColor(android.graphics.Color.LTGRAY)
+                R.id.im_rectangle -> this.setBackgroundColor(android.graphics.Color.rgb(255, 148, 0))
+                R.id.im_circle -> this.setBackgroundColor(android.graphics.Color.rgb(0, 225, 255))
+                R.id.im_line -> this.setBackgroundColor(android.graphics.Color.rgb(118, 0, 145))
             }
         }
         return super.onOptionsItemSelected(item)
