f08f2c13e5b77548428f890c1af8a77d5cf5a5dc
mino <k3000js@naver.com>
2020-06-24 17:54:21 +0900

    51강 Tween Animation

--- app/src/main/java/com/cnm/shw/FrameAnimationActivity.kt
+++ app/src/main/java/com/cnm/shw/FrameAnimationActivity.kt
@@ -1,7 +1,7 @@
 package com.cnm.shw
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_frame_animation.*
 
@@ -9,12 +9,9 @@ class FrameAnimationActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_animation)
-        val ani = iv_animation_run.drawable as AnimationDrawable
+        val ani2 = AnimationUtils.loadAnimation(this,R.anim.rotate)
         bt_animation_start.setOnClickListener {
-            ani.start()
-        }
-        bt_animation_stop.setOnClickListener {
-            ani.stop()
+            iv_animation_run.startAnimation(ani2)
         }
 
     }
